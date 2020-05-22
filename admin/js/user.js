c45993b1e08f901363777bbe7815f932940944a7
wuhu <wuhu@itcast.cn>
2020-05-22 16:59:58 +0800

    08-个人信息编辑页面-图片预览

--- admin/js/user.js
+++ admin/js/user.js
@@ -18,4 +18,38 @@ $(function(){
             $('.user_pic').attr('src',res.data.userPic)
         }
     })
+
+
+    // 一选择好文件就实现文件预览
+    $('#exampleInputFile').on('change',function(){
+        // 1.获取当前的所选择的文件对象
+        let myfile = this.files[0]
+        // 2.根据文件对象生成一个url,它会将这个文件托管到当前页面服务器中，并返回一个服务器中的路径
+        let url = URL.createObjectURL(myfile)
+        // 3.将url赋值给img的src属性
+        $('.user_pic').attr('src',url)
+    })
+
+    // 实现个人信息的编辑
+    // 4、编辑用户信息
+    // 请求地址：/admin/user/edit
+    // 请求方式：post
+    // 请求数据：使用formData提交
+
+    $('.btn-edit').on('click',function(){
+        $.ajax({
+            type:'post',
+            url:BigNew.user_edit,
+            data:new FormData($('#form')[0]),//formdata能不能收集图片数据
+            dataType:'json',
+            processData:false, // 不用ajax来进行数据的处理
+            contentType:false, // 不让ajax来进行数据的编码处理
+            success:function(res){
+                console.log(res)
+                if(res.code == 200){
+                    alert(res.msg)
+                }
+            }
+        })
+    })
 })
