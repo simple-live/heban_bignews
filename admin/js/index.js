572b878383c8b25f3eb0a68d58c30973cbad827a
wuhu <wuhu@itcast.cn>
2020-05-22 12:00:15 +0800

    03-添加ajaxSetup的全局设置，统一处理token传递和token验证失败的错误

--- admin/js/index.js
+++ admin/js/index.js
@@ -12,9 +12,9 @@ $(function(){
         // headers:
         // 请求发送前来设置请求头，这个函数有一个默认参数xhr,它就是那个异步对象
         // xhr.setrequestHeader('Content-type','application...')
-        beforeSend:function(xhr){
-            xhr.setRequestHeader('Authorization',localStorage.getItem('heban_bignews_token'))
-        },
+        // beforeSend:function(xhr){
+        //     xhr.setRequestHeader('Authorization',localStorage.getItem('heban_bignews_token'))
+        // },
         // 请求成功的回调函数
         success:function(res){
             console.log(res)
@@ -24,12 +24,12 @@ $(function(){
             }
         },
         // 使用error专门处理请求失败的场景
-        error:function(err){
-            console.log(err)
-            if(err.statusText == 'Forbidden'){ // Forbidden说明token已过期
-                alert('请求失败，请重新登陆')
-                location.href='./login.html'
-            }
-        }
+        // error:function(err){
+        //     console.log(err)
+        //     if(err.statusText == 'Forbidden'){ // Forbidden说明token已过期
+        //         alert('请求失败，请重新登陆')
+        //         location.href='./login.html'
+        //     }
+        // }
     })
 })
