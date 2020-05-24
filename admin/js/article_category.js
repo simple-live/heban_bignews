83079ec56d8b1c8c2b6afe5aabbfdd7ef5ebac01
wuhu <wuhu@itcast.cn>
2020-05-24 14:32:58 +0800

    12-实现分类数据的编辑，同时使用新增和编辑复用了代码

--- admin/js/article_category.js
+++ admin/js/article_category.js
@@ -1,4 +1,4 @@
-$(function(){
+$(function () {
     // 1.动态渲染分类数据
     // 5、所有文章类别
     // 请求地址：/admin/category/list
@@ -6,18 +6,18 @@ $(function(){
     // 请求参数：无
 
     // 数据初始化
-    function init(){
+    function init() {
         $.ajax({
-            url:BigNew.category_list,
-            dataType:'json',
-            success:function(res){
-             console.log(res)
-             if(res.code == 200){
-                 // 模板中使用的是我们所传递的数据对象的属性名称
-                 $('tbody').html(template('cateTemp',res))
-             }
+            url: BigNew.category_list,
+            dataType: 'json',
+            success: function (res) {
+                console.log(res)
+                if (res.code == 200) {
+                    // 模板中使用的是我们所传递的数据对象的属性名称
+                    $('tbody').html(template('cateTemp', res))
+                }
             }
-         })
+        })
     }
     init()
 
@@ -25,25 +25,38 @@ $(function(){
     // 6、新增文章类别
     // 请求地址：/admin/category/add
     // 请求方式：post
-    $('.btnadd').on('click',function(){
+    $('.btnadd').on('click', function () {
         // 收集数据
         let name = $('#name').val()
         let slug = $('#slug').val()
-        if(name.trim() == "" || slug.trim() == ""){
+        if (name.trim() == "" || slug.trim() == "") {
             alert('请输入名称和别名')
             return
         }
-        // ajax
+        // 判断应该执行的操作
+        if ($('.btnadd').text() == '新增') {
+            // ajax--新增
+           opt(BigNew.category_add,{name,slug})
+        } else {
+            // ajax--编辑,编辑需要传入id参数做为条件
+            opt(BigNew.category_edit,{name,slug,id:$('#id').val()})
+        }
+    })
+
+    // 实现新增或编辑
+    function opt(url,data){
         $.ajax({
-            type:'post',
-            url:BigNew.category_add,
-            data:{
-                name,slug
-            },
-            dataType:"json",
-            success:function(res){
+            type: 'post',
+            url: url,
+            // erialize()
+            // insert into 表 values('',...) --- 报错
+            // insert into 表 values(null,...) --- null更多的是状态值
+            data: data,
+            dataType: "json",
+            success: function (res) {
                 console.log(res)
-                if(res.code == 201){
+                //  新增成功          编辑成功
+                if (res.code == 201 || res.code == 200) {
                     // 提示信息
                     alert(res.msg)
                     // 隐藏模态框
@@ -55,19 +68,20 @@ $(function(){
                 }
             }
         })
-    })
+    }
 
     // 分类数据的编辑--使用事件委托
     // 单击编辑按钮仅仅：弹出模态框  + 数据的默认展示
-    $('tbody').on('click','.btnedit',function(){
-        // data:获取当前元素中的所有自定义属性
+    $('tbody').on('click', '.btnedit', function () {
+        // data:获取当前元素中的所有自定义属性,这个data()获取到的是一个对象
         // 对象解构
-        let {id,name,slug} = $(this).data()
+        let { id, name, slug } = $(this).data()
         // console.log(id,name,slug)
         // let id = $(this).data('id')
+        // let id = $(this).data().id
         // let name = $(this).data('name')
         // let slug = $(this).data('slug')
-        console.log(id,name,slug)
+        console.log(id, name, slug)
         $('#name').val(name)
         $('#slug').val(slug)
         // 以隐藏域的方式存储id
@@ -79,9 +93,9 @@ $(function(){
     })
 
     // 实现单击新增时修改元素的内容
-    $('#xinzengfenlei').on('click',function(){
+    $('#xinzengfenlei').on('click', function () {
         // 顺便修改下页面元素的内容
         $('.modal-title').text('新增分类')
-        $('.btnadd').val('新增')
+        $('.btnadd').text('新增')
     })
 })
